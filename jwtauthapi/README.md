9d4676af8d2847341b55e34f114cdb81d5bf74aa
Erick Mwamodo <erick@merglabs.com>
2018-01-14 17:27:28 +0300

    jwt complete retest needed

--- jwtauthapi/README.md
+++ jwtauthapi/README.md
@@ -1,1 +1,7 @@
 ## Authentification With JSON Web Token
+
+### TODO
+
+* [ ] jwt documentation
+  * [ ] try expiration tokens
+* [ ] Retest with PostMan (don't use the chrome app)
--- jwtauthapi/app.js
+++ jwtauthapi/app.js
@@ -12,21 +12,53 @@ app.get("/api", (req, res) => {
   res.json({ message: "Welcome to the api" });
 });
 
-app.post("/api/post", (req, res) => {
-  res.json({ message: "Post Created..." });
+app.post("/api/posts", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: "Post Created",
+        authData
+      });
+    }
+  });
 });
 
 app.post("/api/login", (req, res) => {
-  //mock user
+  // Mock user "Should be from the database"
   const user = {
     id: 1,
     username: "eric",
     email: "eric@gmail.com"
   };
-  jwt.sign({ user }, "secreatkey", (err, token) => {
+  jwt.sign({ user }, "secretkey", (err, token) => {
     res.json({ token });
   });
 });
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.header["authorization"];
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    // Split at the space
+    const bearer = bearerHeader.split(" ");
+    // Get token from array
+    const bearerToken = bearer[1];
+    // set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+}
 
 app.listen(port, () =>
   console.log(`App is running at port http://localhost:${port}`)
