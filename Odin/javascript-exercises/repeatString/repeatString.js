19e0b18e3c3689a8bbca6bb9ff29a4bf60e235f7
Zeus <erick@merglabs.com>
2018-03-05 09:48:13 +0300

    Odin: Repeat String. No Loops Used. Its Way Cleaner.

--- Odin/javascript-exercises/repeatString/repeatString.js
+++ Odin/javascript-exercises/repeatString/repeatString.js
@@ -1,5 +1,9 @@
-var repeatString = function() {
+var repeatString = function(s, num) {
+  if (isNaN(num) || num < 0) {
+    return "ERROR";
+  } else {
+    return s.repeat(num);
+  }
+};
 
-}
-
-module.exports = repeatString
+module.exports = repeatString;
