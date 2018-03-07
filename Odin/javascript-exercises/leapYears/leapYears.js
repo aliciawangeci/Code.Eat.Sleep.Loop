90f4a099980c1216bccd42e20b2ee0d3614ecdb5
Zeus <erick@merglabs.com>
2018-03-07 13:51:35 +0300

    Odin JavaScript: leapYears.

--- Odin/javascript-exercises/leapYears/leapYears.js
+++ Odin/javascript-exercises/leapYears/leapYears.js
@@ -1,5 +1,16 @@
-var leapYears = function() {
 
+const leapYears = function(year) {
+	if ( year % 4 == 0) {
+		if (year % 100 == 0 && year % 400 !== 0) {
+			return false
+		} else {
+			return true;
+		}
+	} else {
+		return false;
+	}
 }
+
+leapYears(1996)
 
 module.exports = leapYears
