02cb6c00146e2897277fb2deeb027fa1052f0662
Zeus <erick@merglabs.com>
2018-03-07 13:37:24 +0300

    Odin JavaScript: sumAll.

--- Odin/javascript-exercises/sumAll/sumAll.js
+++ Odin/javascript-exercises/sumAll/sumAll.js
@@ -1,5 +1,17 @@
-var sumAll = function() {
-
+const sumAll = function(n1, n2) {
+	let sum = 0;
+	if( n1 < -1 || n2 < -1 || typeof(n1) !== 'number' || typeof(n2) !== 'number' ){
+		return 'ERROR';
+	} else if (n1 < n2 ) {
+		for( i = n1; i <= n2; i++){
+			sum = sum + i;
+		}
+	} else if (n1 > n2) {
+		for( i = n2; i <= n1; i++){
+			sum = sum + i;
+		}
+	}
+	return sum;
 }
 
 module.exports = sumAll
