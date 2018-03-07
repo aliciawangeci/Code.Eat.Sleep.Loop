c47b0185167f22700fbd7f39967f9023acc1af6e
Zeus <erick@merglabs.com>
2018-03-07 14:57:23 +0300

    Odin JavaScript: tempCOnversion

--- Odin/javascript-exercises/tempConversion/tempConversion.js
+++ Odin/javascript-exercises/tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-var ftoc = function() {
-  
+
+const ftoc = function(fahrenheit) {
+	const Round = (num, pre) => {
+		let factor = Math.pow(10, pre);
+		return Math.round(num * factor) / factor;
+	}
+	let celsius = (fahrenheit - 32 ) * 5/9
+	return Round(celsius, 1);
 }
 
-var ctof = function() {
-  
+const ctof = function(celsius) {
+	const Round = (num, pre) => {
+		let factor = Math.pow(10, pre);
+		return Math.round(num * factor) / factor;
+	}
+	let fahrenheit = celsius * 9/5 + 32;
+	return Round(fahrenheit, 1);
+  // T(°F) = T(°C) × 9/5 + 32
 }
 
 module.exports = {
