7e0bbb1e933590d263d94f6f89363589be1caba0
Zeus <erick@merglabs.com>
2018-03-07 13:11:17 +0300

    Odin JavaScript: remove from array.

--- Odin/javascript-exercises/removeFromArray/removeFromArray.js
+++ Odin/javascript-exercises/removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-var removeFromArray = function() {
 
+const removeFromArray = function(array, ...args) {
+	array = Array.apply(null, array);
+	for (let i = 0; i < args.length; i++) {
+		let index = array.indexOf(args[i]);
+		if (index > -1) {
+ 			array.splice(index, 1);
+		}
+	}
+	return array;
 }
+
+removeFromArray([1,2,3], 1, 2, 3);
 
 module.exports = removeFromArray
