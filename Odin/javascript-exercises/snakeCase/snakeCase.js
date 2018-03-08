2c4a87fbe4fa2773205b12a7fc10e611d8236dd1
Zeus <erick@merglabs.com>
2018-03-08 17:00:49 +0300

    Odin JavaScript: snakecase

--- Odin/javascript-exercises/snakeCase/snakeCase.js
+++ Odin/javascript-exercises/snakeCase/snakeCase.js
@@ -1,5 +1,9 @@
-var snakeCase = function() {
+const snakeCase = str => {
+	return str
+		.replace(/([a-z](?=[A-Z]))/g, "$1 ")
+		.toLowerCase()
+		.replace(/[ -]/g, "_")
+		.replace(/[\W]/g, "");
+};
 
-}
-
-module.exports = snakeCase
+module.exports = snakeCase;
