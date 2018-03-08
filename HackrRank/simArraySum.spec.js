614efd8e9242b6a6ff2dceaf7c720fb07b593d2e
Zeus <erick@merglabs.com>
2018-03-08 11:53:55 +0300

    Hackerrank tests with jasmine

--- HackrRank/simArraySum.spec.js
+++ HackrRank/simArraySum.spec.js
@@ -1,19 +1,19 @@
 const calculator = require("./simArraySum");
 
-describe('sum', function() {
-	it('computes the sum of an empty array', function() {
+describe("sum", function() {
+	it("computes the sum of an empty array", function() {
 		expect(calculator.sum(0, [])).toEqual(0);
 	});
 
-	it('computes the sum of an array of one number', function() {
+	it("computes the sum of an array of one number", function() {
 		expect(calculator.sum(1, [7])).toEqual(7);
 	});
 
-	it('computes the sum of an array of two numbers', function() {
-		expect(calculator.sum(2, [7,11])).toEqual(18);
+	it("computes the sum of an array of two numbers", function() {
+		expect(calculator.sum(2, [7, 11])).toEqual(18);
 	});
 
-	it('computes the sum of an array of many numbers', function() {
-		expect(calculator.sum(5, [1,3,5,7,9])).toEqual(25);
+	it("computes the sum of an array of many numbers", function() {
+		expect(calculator.sum(5, [1, 3, 5, 7, 9])).toEqual(25);
 	});
 });
