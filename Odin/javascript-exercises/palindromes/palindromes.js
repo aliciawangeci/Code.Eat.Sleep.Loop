1eb62e1a4bdd849a355447b16b63efd619f383e3
Zeus <erick@merglabs.com>
2018-03-08 15:20:32 +0300

    Odin JavaScript: palindromes.

--- Odin/javascript-exercises/palindromes/palindromes.js
+++ Odin/javascript-exercises/palindromes/palindromes.js
@@ -1,5 +1,14 @@
-var palindromes = function() {
+const palindromes = string => {
+	string = string.toLowerCase().replace(/[" ".,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+	for (let i = string.length - 1; i >= 0; i--) {
+		for (let j = 0; j < string.length; j++) {
+			if (string[i] == string[j]) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;
