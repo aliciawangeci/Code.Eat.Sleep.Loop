49ad81eaa8f5863affadbc397540c608bf945eef
Zeus <erick@merglabs.com>
2018-03-09 14:56:24 +0300

    Javascript odin: caesar

--- Odin/javascript-exercises/caesar/caesar.js
+++ Odin/javascript-exercises/caesar/caesar.js
@@ -1,5 +1,57 @@
-var caesar = function() {
+// function caesar(str, key) {
+// 	let newStr = str
+// 		.toUpperCase()
+// 		.replace(/[A-Z]/g, c => String.fromCharCode((c.charCodeAt(0) - 65 + key) % 26 + 65))
+// 		.toLowerCase();
 
-}
+// 	return newStr.charAt(0).toUpperCase() + newStr.slice(1);
+// }
 
-module.exports = caesar
+// function cipher(str, key) {
+//   let newStr = str
+//     .toUpperCase()
+//     .replace(/[A-Z]/g, c =>
+//       String.fromCharCode((c.charCodeAt(0) - 65 + key) % 26 + 65)
+//     )
+//     .toLowerCase();
+//   return newStr.charAt(0).toUpperCase() + newStr.slice(1);
+// }
+
+// function caesar(str, key) {
+//   wordArray = str.split(" ");
+//   let ciphered = [];
+//   if (wordArray.length > 1) {
+//     for (i = 0; i < wordArray.length; i++) {
+//       ciphered.push(cipher(wordArray[i], key));
+//     }
+//   } else {
+//     return cipher(str, key);
+//   }
+//   console.log(ciphered.join().replace(",", " "));
+// }
+
+// caesar("Hello, World!", 5);
+
+const caesar = function(string, shift) {
+  return string
+    .split("")
+    .map(char => shiftChar(char, shift))
+    .join("");
+};
+
+const codeSet = code => (code < 97 ? 65 : 97);
+
+const mod = (n, m) => (n % m + m) % m;
+
+const shiftChar = (char, shift) => {
+  const code = char.charCodeAt();
+
+  if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+    return String.fromCharCode(
+      mod(code + shift - codeSet(code), 26) + codeSet(code)
+    );
+  }
+  return char;
+};
+
+module.exports = caesar;
