c4742409c0b2c49eda31110d9900e8f36a713961
Zeus <erick@merglabs.com>
2018-03-10 12:01:37 +0300

    Javascript odin: fibonnacci

--- Odin/javascript-exercises/fibonacci/fibonacci.js
+++ Odin/javascript-exercises/fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-var fibonacci = function() {
+const fibonacci = function(number) {
+	if (number < 0) return "OOPS";
+	let a = 0;
+	let b = 1;
+	for (let i = 1; i < number; i++) {
+		const temp = b;
+		b = a + b;
+		a = temp;
+	}
+	return b;
+};
 
-}
-
-module.exports = fibonacci
+module.exports = fibonacci;
